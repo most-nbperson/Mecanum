1f6d107c07aab9c2f277fad85f956a49bec6560b
NBPERSON <34833364+most-nbperson@users.noreply.github.com>
2019-10-19 13:33:08 +0800

    Initial commit

---
+++ Inc/pid.h
@@ -0,0 +1,35 @@
+#ifndef _PID_H_
+#define _PID_H_
+
+typedef struct
+{
+  float Kp;                       //比例系数Proportional
+  float Ki;                       //积分系数Integral
+  float Kd;                       //微分系数Derivative
+ 
+  float error;               			//当前误差
+  float pre_error;           			//前一次误差 e(k-1)
+  float pre_pre_error;        		//再前一次误差 e(k-2)
+  float LocSum;                   //累计积分位置
+}PID_LocTypeDef;
+
+
+typedef struct
+{
+  float Kp;                       //比例系数Proportional
+  float Ki;                       //积分系数Integral
+  float Kd;                       //微分系数Derivative
+ 
+  float error;              			//当前误差
+  float pre_error;         				//前一次误差 e(k-1)
+  float pre_pre_error;         		//再前一次误差 e(k-2)
+	
+	int expect_value;
+}PID_IncTypeDef;
+
+void Speed_PID_Init(int set_speed);
+float Speed_Adjust(int now_speed);
+
+
+
+#endif
---
+++ Src/pid.c
@@ -0,0 +1,23 @@
+#include "pid.h"
+#include "main.h"
+
+PID_IncTypeDef spid;
+PID_IncTypeDef *speed_pid=&spid;
+
+void Speed_PID_Init(int set_speed)
+{
+	speed_pid->Kp=100;
+	speed_pid->Ki=0;
+	speed_pid->Kd=0;
+	speed_pid->expect_value=set_speed;
+}
+
+float Speed_Adjust(int now_speed)
+{
+	float speed_inc;
+	speed_pid->error=speed_pid->expect_value-now_speed;
+	speed_inc=(speed_pid->Kp*speed_pid->error)-(speed_pid->Ki*speed_pid->pre_error)+(speed_pid->Kd*speed_pid->pre_pre_error);
+	speed_pid->pre_pre_error=speed_pid->pre_error;
+	speed_pid->pre_error=speed_pid->error;
+	return speed_inc;
+}
